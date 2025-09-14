d1a2d5908202f2529886e07c3e65118c821cba98
falkTX <falktx@falktx.com>
2025-09-14 18:08:48 +0200

    Fix build with latest freetype
    Signed-off-by: falkTX <falktx@falktx.com>

--- modules/juce_graphics/native/juce_freetype_Fonts.cpp
+++ modules/juce_graphics/native/juce_freetype_Fonts.cpp
@@ -353,8 +353,8 @@ private:
     bool getGlyphShape (Path& destShape, const FT_Outline& outline, const float scaleX)
     {
         const float scaleY = -scaleX;
-        const short* const contours = outline.contours;
-        const char* const tags = outline.tags;
+        const typeof(outline.contours) contours = outline.contours;
+        const typeof(outline.tags) tags = outline.tags;
         const FT_Vector* const points = outline.points;
 
         for (int c = 0; c < outline.n_contours; ++c)
